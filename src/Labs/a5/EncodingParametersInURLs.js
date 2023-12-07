3f820581f9d264fe313a30d5cc2cbaa3ffbd7b87
huangz826 <huang.zix@northeastern.edu>
2023-12-07 13:58:25 -0800

    fix bugs for lab 5

--- src/Labs/a5/EncodingParametersInURLs.js
+++ src/Labs/a5/EncodingParametersInURLs.js
@@ -5,20 +5,21 @@ function EncodingParametersInURLs() {
   const [a, setA] = useState(34);
   const [b, setB] = useState(23);
   const [result, setResult] = useState(0);
+  const API_BASE = process.env.REACT_APP_API_BASE;
   const fetchSum = async (a, b) => {
-    const response = await axios.get(`http://localhost:4000/a5/add/${a}/${b}`);
+    const response = await axios.get(`${API_BASE}/a5/add/${a}/${b}`);
     setResult(response.data);
   };
   const fetchSubtraction = async (a, b) => {
     const response = await axios.get(
-      `http://localhost:4000/a5/subtract/${a}/${b}`
+      `${API_BASE}/a5/subtract/${a}/${b}`
     );
     setResult(response.data);
   };
 
   const [welcome, setWelcome] = useState("");
   const fetchWelcome = async () => {
-    const response = await axios.get("http://localhost:4000/a5/welcome");
+    const response = await axios.get(`${API_BASE}/a5/welcome`);
     setWelcome(response.data);
   };
   useEffect(() => {
@@ -64,26 +65,26 @@ function EncodingParametersInURLs() {
       </button>
       <h3>Path Parameters</h3>
       <a
-        href={`http://localhost:4000/a5/add/${a}/${b}`}
+        href={`${API_BASE}/a5/add/${a}/${b}`}
         className="btn btn-primary"
       >
         Add {a} + {b}
       </a>
       <a
-        href={`http://localhost:4000/a5/subtract/${a}/${b}`}
+        href={`${API_BASE}/a5/subtract/${a}/${b}`}
         className="btn btn-danger"
       >
         Substract {a} - {b}
       </a>
       <h3>Query Parameters</h3>
       <a
-        href={`http://localhost:4000/a5/calculator?operation=add&a=${a}&b=${b}`}
+        href={`${API_BASE}/a5/calculator?operation=add&a=${a}&b=${b}`}
         className="btn btn-primary"
       >
         Add {a} + {b}
       </a>
       <a
-        href={`http://localhost:4000/a5/calculator?operation=subtract&a=${a}&b=${b}`}
+        href={`${API_BASE}/a5/calculator?operation=subtract&a=${a}&b=${b}`}
         className="btn btn-danger"
       >
         Substract {a} - {b}
--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -7,7 +7,7 @@ function Assignment5() {
     <div>
       <h1>Assignment 5</h1>
       <div className="list-group">
-        <a href="http://localhost:4000/a5/welcome" className="list-group-item">
+        <a href="http://localhost:4000/api/a5/welcome" className="list-group-item">
           Welcome
         </a>
       </div>
